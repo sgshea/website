e91050215ab1ff9856d13d11eb58515a53af797e
sgshea <sammys512@yahoo.com>
2023-08-20 19:54:56 -0400

    feat: minor additions, add ecom project images

--- src/website/about.cljs
+++ src/website/about.cljs
@@ -1,5 +1,17 @@
 (ns website.about)
 
+(defn carousel-item [image-url]
+  [:div {:class "carousel-item"}
+   [:img {:src image-url :class "rounded-box w-1/2"}]])
+
+(defn project-template [prose images]
+  [:div {:class "hero bg-base-200"}
+   [:div {:class "hero-content flex-row"}
+    [:div {:class "mr-10"}
+     prose]
+    [:div {:class "carousel rounded-box p-4 space-x-4 carousel-center"}
+     (map carousel-item images)]]])
+
 (defn nav-bar []
   [:div {:class "navbar bg-primary text-primary-content"}
    [:div {:class "flex-1"}
@@ -14,7 +26,7 @@
        [:li [:a "Clojure FullStack"]]]]]]])
 
 (defn main-paragraph []
-  [:article {:class "prose"}
+  [:article {:class "prose mb-10"}
    [:h1
     "About"]
    [:p
@@ -22,8 +34,25 @@
      I am interested in open-source spaces such as Linux, as well as topics such as artifical intelligence and exploring different programming languages or paradigms!"]
    ])
 
+(def neat-project
+  [project-template
+   [:article {:class "prose"}
+    [:h2 "Rust NEAT Implementation"]
+    [:p "Implementation of the Neuroevolution of Augmenting Topologies (NEAT) machine learning algorithm in the Rust programming language."
+     ]]])
+
+(def clojure-fullstack
+  [project-template
+   [:article {:class "prose"}
+    [:h2 "Clojure Fullstack Ecommerce Application"]
+    [:p "Fullstack application written in Clojure/ClojureScript."]]
+   '("images/ecom_1.png" "images/ecom_2.png" "images/ecom_3.png" "images/ecom_4.png" "images/ecom_5.png" "images/ecom_6.png")])
+
 (defn main []
 [:div
  [nav-bar]
- [:div {:class "flex justify-center items-center mt-5"}
-  [main-paragraph]]])
+ [:div {:class "flex flex-col items-center mt-5"}
+  [main-paragraph]
+  neat-project
+  clojure-fullstack
+  ]])
