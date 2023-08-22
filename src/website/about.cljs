8afc8e7bad66c695d8418e632f8c1c540f99f3e8
sgshea <sammys512@yahoo.com>
2023-08-22 18:59:34 -0400

    feat: work on projects area

--- src/website/about.cljs
+++ src/website/about.cljs
@@ -4,8 +4,8 @@
   [:div {:class "carousel-item rounded-box h-fit w-fit"}
    [:img {:src image-url}]])
 
-(defn project-template [id title text images]
-   [:div {:class "card container mx-auto lg:card-side bg-primary mb-5"}
+(defn project-template [id link title text images? images]
+   [:div {:class "card container mx-auto lg:card-side bg-neutral mb-5"}
    [:figure
     [:img {:src (first images)}]]
     [:div {:class "card-body"}
@@ -13,17 +13,26 @@
      text
      [:div {:class "card-actions justify-end"}
       [:button {:class "btn btn-secondary"
-                :on-click #((-> js/document
-                                (.getElementById id)
-                                (.showModal)))} "More Images"]]
-      [:dialog {:id id :class "modal"}
-       [:form {:method "dialog" :class "modal-box max-w-none h-3/5 w-2/3"}
-        [:div {:class "carousel bg-neutral rounded-box"}
-         (map carousel-item images)]
-        [:div {:class "modal-action"}
-         [:button {:class "btn btn-secondary"} "Close"]]
-        ]]
-     ]])
+                :href link}
+       [:a {:href link} "GitHub"]
+       ;; GitHub button
+       [:svg {:xmlns "http://www.w3.org/2000/svg" :fill "bg-primary" :view-box "0 0 24 24" :class "h-6 w-6"}
+        [:path {:fill-rule "evenodd" :d "M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                :clip-rule "evenodd"}]]]
+      (if images? 
+        [:div
+         [:button {:class "btn btn-secondary"
+                   :on-click #((-> js/document
+                                   (.getElementById id)
+                                   (.showModal)))} "More Images"]
+         [:dialog {:id id :class "modal"}
+          [:form {:method "dialog" :class "modal-box max-w-none h-3/5 w-2/3"}
+           [:div {:class "carousel bg-neutral rounded-box"}
+            (map carousel-item images)]
+           [:div {:class "modal-action"}
+            [:button {:class "btn btn-secondary"} "Close"]]]]]
+        nil)
+      ]]])
 
 (defn nav-bar []
   [:div {:class "navbar bg-primary text-primary-content"}
@@ -32,8 +41,8 @@
      "Sammy Shea"]]
    [:div {:class "flex-none"}
     [:div {:class "dropdown dropdown-bottom dropdown-end"}
-     [:label {:tabindex "0" :class "btn btn-ghost m-1"} "Projects"]
-     [:li {:tabindex "0" :class "dropdown-content menu shadow mt-3 z-[1] p-2 rounded-box w-52"}
+     [:label {:tabIndex "0" :class "btn btn-ghost m-1"} "Projects"]
+     [:li {:tabIndex "0" :class "dropdown-content bg-primary menu shadow mt-3 z-[1] p-2 rounded-box w-52"}
       [:ul
        [:li [:a "Rust NEAT"]]
        [:li [:a "Clojure FullStack"]]]]]]])
@@ -50,17 +59,28 @@
 (def neat-project
   [project-template
    "RNI"
-  "Rust NEAT Implementation"
+   "https://github.com/sgshea/neat"
+   "Rust NEAT Implementation"
    [:article {:class "prose"}
-    [:p "Implementation of the Neuroevolution of Augmenting Topologies (NEAT) machine learning algorithm in the Rust programming language."
-     ]]])
+    [:p "Implementation of the Neuroevolution of Augmenting Topologies (NEAT) machine learning algorithm in the Rust programming language."]
+    [:h4 "Problem Statement"]
+    [:p "This project was primarily taken on in an interest to learn the Rust language. At the same time as I was going throught the Rust Book,
+         I came across the idea of neuroevolution in machine learning. The NEAT algorithm is originally described in a paper from 2002 and focuses
+         on evolving neural network structures. There are several different libraries available which implement this and related libraries, but no
+         very mature ones for Rust and I had the idea to implement a simple library as a learning experiment."]
+    [:p "The library in it's current state implements the paper's ideas as well as some tweaks that are mentioned in newer literature enough to solve
+         the basic XOR function. From this project I gained a good understanding of how Rust programs can be structured and of different language elements
+         through the couple of rewrites of the project."]] 
+   false])
 
 (def clojure-fullstack
   [project-template
    "CFS"
-  "Clojure Fullstack Ecommerce Application"
+   "https://github.com/sgshea/ecommerce"
+   "Clojure Fullstack Ecommerce Application"
    [:article {:class "prose"}
     [:p "Fullstack application written in Clojure/ClojureScript."]]
+   true
    '("images/ecom_1.png" "images/ecom_2.png" "images/ecom_3.png" "images/ecom_4.png" "images/ecom_5.png" "images/ecom_6.png")])
 
 (defn main []
